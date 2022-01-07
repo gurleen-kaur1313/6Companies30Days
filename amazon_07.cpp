8c0a538a8d9fc9427fa31f07b2eda44fa767020b
gurleen-kaur1313 <69715084+gurleen-kaur1313@users.noreply.github.com>
2022-01-07 17:58:29 +0530

    question 7 amazon

---
+++ amazon_07.cpp
@@ -0,0 +1,28 @@
+/* problem link: https://practice.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1# */
+
+
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		     unordered_map<char,int>mp;
+             vector<char>v;
+             string s="";
+             int n =A.length();
+
+             for(int i=0;i<n;i++){
+                if(mp.find(A[i])!=mp.end()){
+                auto it =find(v.begin(),v.end(),A[i]);
+                if(it!=v.end())v.erase(it);
+                }
+                else
+                {
+                mp[A[i]]++;
+                v.push_back(A[i]);
+                }
+                if(v.size()==0)s+='#';
+                else s+=v[0];
+             }
+             return s;
+		}
+
+};
