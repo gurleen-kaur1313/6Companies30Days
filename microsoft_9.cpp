7fcb996c2b1d2503951585c03733c0f362bec2ce
gurleen-kaur1313 <69715084+gurleen-kaur1313@users.noreply.github.com>
2022-01-12 16:52:08 +0530

    question 9 microsoft

---
+++ microsoft_9.cpp
@@ -0,0 +1,19 @@
+/* problem link: https://practice.geeksforgeeks.org/problems/count-number-of-subtrees-having-given-sum/1/# */
+
+
+int checkSum(Node* root,int sum,int &cnt)
+{
+    if(root==NULL)
+    return 0;
+
+    int sm = root->data+checkSum(root->left,sum,cnt)+checkSum(root->right,sum,cnt);
+    if(sm==sum)
+    cnt++;
+    return sm;
+}
+int countSubtreesWithSumX(Node* root, int X)
+{
+	int cnt=0;
+	checkSum(root,X,cnt);
+	return cnt;
+}
