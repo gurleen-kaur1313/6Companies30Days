d4f9a50ea1098313ba9bb6b74b0cad2b76491412
gurleen-kaur1313 <69715084+gurleen-kaur1313@users.noreply.github.com>
2022-01-16 11:27:02 +0530

    question 14 microsoft

---
+++ microsoft_14.cpp
@@ -0,0 +1,24 @@
+/* problem link: https://practice.geeksforgeeks.org/problems/minimum-number-of-steps-to-reach-a-given-number5234/1/# */
+
+
+
+class Solution{
+public:
+    int minSteps(int D){
+        int steps = 0, sum = 0;
+        while(true){
+            sum = sum + steps;
+            steps++;
+            if(sum == D){
+                return steps-1;
+                break;
+            }
+            if(sum > D and (sum-D)%2 == 0){
+                return steps-1;
+                break;
+            }
+        }
+        return 0;
+    }
+};
+
