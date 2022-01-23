cc07d40dc977afb513b734d21cd9260299d81e38
gurleen-kaur1313 <69715084+gurleen-kaur1313@users.noreply.github.com>
2022-01-23 18:51:42 +0530

    question 15 intuit

---
+++ intuit_15.cpp
@@ -0,0 +1,38 @@
+/* problem link: https://leetcode.com/problems/koko-eating-bananas/ */
+
+
+
+
+class Solution {
+public:
+    bool safe(vector<int> &piles, int k, int h)
+    {
+        int ti me = 0;
+        for (int x:piles)
+        {
+            time += ceil((long double)x/k);
+        }
+        return time<=h;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int res = INT_MAX;
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+        while(low<=high)
+        {
+            int mid = low + (high-low )/ 2;
+            if (safe(piles, mid, h))
+            {
+                res = mid;
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return res;
+    }
+
+};
