17a20f307adef0817f539a2e5ee60eda8206c7c0
gurleen-kaur1313 <69715084+gurleen-kaur1313@users.noreply.github.com>
2022-01-28 13:13:47 +0530

    question 5 walmart

---
+++ walmart_05.cpp
@@ -0,0 +1,26 @@
+/* problem link: https://practice.geeksforgeeks.org/problems/transform-to-sum-tree/1/# */
+
+
+
+class Solution {
+  public:
+
+    // Convert a given tree to a tree where every node contains sum of values of
+    // nodes in left and right subtrees in the original tree
+    int sum(Node* root)
+    {
+        if(root == NULL)return 0;
+        return sum(root->left)+sum(root->right)+root->data;
+    }
+    void toSumTree(Node *root)
+    {
+        // Your code here
+        if(root == NULL)return;
+
+        int s = sum(root)-root->data;
+        root->data = s;
+
+        toSumTree(root->left);
+        toSumTree(root->right);
+    }
+};
